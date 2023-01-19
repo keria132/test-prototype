f0828f1de651322b3002724af128f98fe93b3c84
Kirill Mikeylov <keria132@protonmail.com>
2023-01-19 06:40:58 +0300

    Added legacy support

--- development/src/App.tsx
+++ development/src/App.tsx
@@ -42,15 +42,17 @@ const Card = (props: any) => {
                     <path fillRule="evenodd" strokeWidth="4px" strokeLinecap="butt" strokeLinejoin="miter" fill="rgb(242, 242, 242)"
                     d="M310.000,482.000 L14.000,482.000 C7.373,482.000 2.000,476.627 2.000,470.000 L2.000,45.000 L45.000,2.000 L310.000,2.000 C316.627,2.000 322.000,7.372 322.000,14.000 L322.000,470.000 C322.000,476.627 316.627,482.000 310.000,482.000 Z"/>
                 
-                    <foreignObject className='card'>
-                        <small className='card__quietName'>Сказочное заморское яство</small>
-                        <h2 className='card__productName'>Нямушка</h2>
-                        <h3 className='card__productFeature'>{props.productFeature}</h3>
-                        <small className='card__productQuantity'>{props.productQuantity}</small>
-                        <small className='card__productGifts'>{props.productGifts}</small>
-                        <small className='card__productExtraText'>{props.productExtraText}</small>
-                        <img className='card__catImage' src={catImage}/>
-                        <div className='card__productWeight' id='weight'><h2>{props.productWeight}</h2> <p>кг</p></div>
+                    <foreignObject className='card-container'>
+                        <div className='card'>
+                            <small className='card__quietName'>Сказочное заморское яство</small>
+                            <h2 className='card__productName'>Нямушка</h2>
+                            <h3 className='card__productFeature'>{props.productFeature}</h3>
+                            <small className='card__productQuantity'>{props.productQuantity}</small>
+                            <small className='card__productGifts'>{props.productGifts}</small>
+                            <small className='card__productExtraText'>{props.productExtraText}</small>
+                            <img className='card__catImage' src={catImage}/>
+                            <div className='card__productWeight' id='weight'><h2>{props.productWeight}</h2> <p>кг</p></div>
+                        </div>
                     </foreignObject>
                 
                 </svg>
